94bf654e2a737b6d3bb079058b234838162112b7
Rezza Priatna <58212770+rezzaapr@users.noreply.github.com>
2020-05-23 04:06:35 +0700

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,50 @@
+ban = """   
+               ___
+              | + | 
+          ____| + |___  FB  : Simple Python Programming
+          |    O O    | Git : github.com/rezzaapr
+          |___  -   __| 
+              |   | 
+              |___| 
+          Instagram Tools
+
+[1] User Profile Information
+[2] Download Video Post
+[3] Download Image Post
+[4] Exit
+
+"""
+
+ban1 = """   
+               ___
+              | + | 
+          ____| + |___  FB  : Simple Python Programming
+          |    O O    | Git : github.com/rezzaapr
+          |___  -   __| 
+              |   | 
+              |___| 
+      User Profile Information
+"""
+
+ban2 = """   
+               ___
+              | + | 
+          ____| + |___  FB  : Simple Python Programming
+          |    O O    | Git : github.com/rezzaapr
+          |___  -   __| 
+              |   | 
+              |___| 
+         Video Downloader
+"""
+
+ban3 = """   
+               ___
+              | + | 
+          ____| + |___  FB  : Simple Python Programming
+          |    O O    | Git : github.com/rezzaapr
+          |___  -   __| 
+              |   | 
+              |___| 
+         Image Downloader
+"""
+
